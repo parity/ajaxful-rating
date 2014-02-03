394f639881a01969ad5c4c6a0be1c00f955135c3
Andrew S. Brown <andrew@helloenvoy.com>
2014-02-03 14:45:34 -0800

    use scopes instead of conditions

--- lib/axr/model.rb
+++ lib/axr/model.rb
@@ -18,8 +18,8 @@ module AjaxfulRating # :nodoc:
     #     ajaxful_rateable :stars => 10, :cache_column => :custom_column
     #   end
     def ajaxful_rateable(options = {})
-      has_many :rates_without_dimension, :as => :rateable, :class_name => 'Rate',
-        :dependent => :destroy, :conditions => {:dimension => nil}
+      has_many :rates_without_dimension, -> { where(dimension: nil) }, :as => :rateable, :class_name => 'Rate',
+        :dependent => :destroy
       has_many :raters_without_dimension, :through => :rates_without_dimension, :source => :rater
 
       class << self
@@ -39,8 +39,7 @@ module AjaxfulRating # :nodoc:
 
       if options[:dimensions].is_a?(Array)
         options[:dimensions].each do |dimension|
-          has_many :"#{dimension}_rates", :dependent => :destroy,
-            :conditions => {:dimension => dimension.to_s}, :class_name => 'Rate', :as => :rateable
+          has_many :"#{dimension}_rates", -> { where(dimension: dimension.to_s) }, :dependent => :destroy, :class_name => 'Rate', :as => :rateable
           has_many :"#{dimension}_raters", :through => :"#{dimension}_rates", :source => :rater
 
           axr_config(dimension).update(options)
